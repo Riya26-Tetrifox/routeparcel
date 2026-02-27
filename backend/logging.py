609b201f36e747573c003813b435ca28ebfff05a
Riya26-Tetrifox <riya.kasaudhan@tetrifox.com>
2026-02-27 15:23:09 +0530

    connection string

--- backend/logging.py
+++ backend/logging.py
@@ -1,8 +1,11 @@
 from azure.monitor.opentelemetry import configure_azure_monitor
 import logging
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
+import os
+from dotenv import load_dotenv
+load_dotenv()
 configure_azure_monitor(
-    connection_string="InstrumentationKey=7abaa987-fa18-4aed-a339-67422ecc0416;IngestionEndpoint=https://centralindia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://centralindia.livediagnostics.monitor.azure.com/;ApplicationId=ec470a25-1e53-43aa-b4cc-c3c5436768a4"
+    connection_string=os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
 )
 LoggingInstrumentor().instrument(set_logging_format=True)
 logging.basicConfig(level=logging.INFO)
