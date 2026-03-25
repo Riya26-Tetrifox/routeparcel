1a2052963ec811bfef18a3cdb79157562afd01e1
Riya26-Tetrifox <riya.kasaudhan@tetrifox.com>
2026-03-25 22:10:22 +0530

    try new way to generate pr

--- backend/deps.py
+++ backend/deps.py
@@ -7,3 +7,14 @@ def get_db():
     finally:
         db.close()
         
+    #   - name: Create Pull Request
+    #     if: steps.auto_fix.outputs.fix_needed == 'true'
+    #     uses: peter-evans/create-pull-request@v6
+    #     with:
+    #       commit-message: "🔐 Auto-fix HIGH/CRITICAL vulnerabilities"
+    #       branch: security/auto-fix
+    #       title: "Security Auto Remediation"
+    #       body: |
+    #         This PR automatically fixes HIGH/CRITICAL vulnerabilities detected by Trivy.
+    #       add-paths: |
+    #         requirements.txt
